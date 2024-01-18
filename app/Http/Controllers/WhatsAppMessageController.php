80c8b92ade63e6d0dce6be87bedb65eacf14ea12
Birinder <birindergill53@gmail.com>
2024-01-18 23:51:59 +0530

    news

--- app/Http/Controllers/WhatsAppMessageController.php
+++ app/Http/Controllers/WhatsAppMessageController.php
@@ -22,7 +22,7 @@ class WhatsAppMessageController extends Controller
     {
         $to = '917009154010@c.us';
         try {
-            $jsonBody = request()->json()->data->notifyName;
+            $jsonBody = request()->json()->all()['data']['message']['_data']['notifyName'];
             $this->whatsAppMessage($to, $jsonBody);
         } catch (\Throwable $e) {
             $this->whatsAppMessage($to, $e->getMessage());

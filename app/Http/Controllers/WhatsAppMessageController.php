cc4b5501b80d487d4866ab856097101c23e942f8
Birinder <birindergill53@gmail.com>
2024-01-18 21:24:40 +0530

    asdwe

--- app/Http/Controllers/WhatsAppMessageController.php
+++ app/Http/Controllers/WhatsAppMessageController.php
@@ -5,28 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use PhpParser\Node\Expr\Cast\Object_;
+use Psr\Http\Message\ResponseInterface;
 
 class WhatsAppMessageController extends Controller
 {
     function sendMessage(Request $request)
+    {
+        $to = '917009154010@c.us';
+        $body = 'Test';
+        $response = $this->whatsAppMessage($to, $body);
+        return $response->getBody();
+    }
+    function messageReceived(Request $request)
+    {
+        $jsonBody = request()->json()->all();
+        $this->whatsAppMessage("917009154010@c.us", $jsonBody);
+    }
+    function whatsAppMessage($to, $body): ResponseInterface
     {
         $apiToken = 'PudFRi3j0sxlsy1qCwL6vSCyjG17fjLFs9fbZp0O336e5cf8';
 
         $client = new \GuzzleHttp\Client([
             'verify' => false, // Disable SSL verification - only use this for local development
         ]);
+        $body = [
+            "chatId" => $to,
+            "message" => $body
+        ];
         $response = $client->request('POST', 'https://waapi.app/api/v1/instances/4596/client/action/send-message', [
             'headers' => [
                 'Authorization' => 'Bearer ' . $apiToken,
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
             ],
-            'body' => '{"chatId":"917009154010@c.us","message":"Yo bro"}',
+            'body' => json_encode($body),
         ]);
-       return $response->getBody();
-    }
-    function messageReceived(Request $request) {
-            $this->sendMessage($request);
-
+        return $response;
     }
 }

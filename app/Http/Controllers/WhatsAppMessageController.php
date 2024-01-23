bb8f90592993002eeb661e76680c019293b4745b
Birinder <birindergill53@gmail.com>
2024-01-24 01:23:31 +0530

    news

--- app/Http/Controllers/WhatsAppMessageController.php
+++ app/Http/Controllers/WhatsAppMessageController.php
@@ -100,7 +100,7 @@ class WhatsAppMessageController extends Controller
             }
         } catch (\Throwable $e) {
             report($e);
-            $this->msService->sendTestMessage($e->getMessage());
+            // $this->msService->sendTestMessage($e->getMessage());
         }
     }
 

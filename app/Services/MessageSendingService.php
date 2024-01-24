efb366139bf9c09454a1803d56f2c814394a66a1
Birinder <birindergill53@gmail.com>
2024-01-24 21:41:46 +0530

    news

--- app/Services/MessageSendingService.php
+++ app/Services/MessageSendingService.php
@@ -23,38 +23,52 @@ class MessageSendingService
     {
         $to = $this->rcService->getFrom();
         $toSend = $this->rcService->getFirstMessage($personName);
-        $this->waService->sendWhatsAppMedia($to,'https://electricsuitcase.tech/storage/bVideo.mp4', $toSend);
-        $this->waService->sendWhatsAppMedia($to,'https://electricsuitcase.tech/storage/b1.jpeg');
-        $this->waService->sendWhatsAppMedia($to,'https://electricsuitcase.tech/storage/b2.jpeg');
-        return $this->waService->sendWhatsAppMedia($to,'https://electricsuitcase.tech/storage/b3.jpeg');
+        $this->waService->sendWhatsAppMedia($to, 'https://electricsuitcase.tech/storage/bVideo.mp4', $toSend);
+        $this->waService->sendWhatsAppMedia($to, 'https://electricsuitcase.tech/storage/b1.jpeg');
+        $this->waService->sendWhatsAppMedia($to, 'https://electricsuitcase.tech/storage/b2.jpeg');
+        return $this->waService->sendWhatsAppMedia($to, 'https://electricsuitcase.tech/storage/b3.jpeg');
     }
 
     function giveQueryResponse(GeneralQuery $query)
     {
-        if($query == GeneralQuery::PRICE) return $this->sendDiscountedPriceMessage();
+        if ($query == GeneralQuery::PRICE) return $this->sendDiscountedPriceMessage();
         $response = $this->rcService->getQueryResponse($query);
-        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(),$response);
+        if ($query == GeneralQuery::ADDRESS) {
+            if (PriceLog::where(['to' => $this->rcService->getFrom(), 'address' => 1])->exists()) return;
+            PriceLog::updateOrCreate(
+                ['to' => $this->rcService->getFrom()],
+                [
+                    'address' => 1
+                ]
+            );
+        }
+        $this->waService->sendWhatsAppMessage($this->rcService->getFrom(), $response);
     }
 
 
     function answerPriceDiscussion(PriceQuery $priceQuery)
     {
         $response = $this->rcService->getPriceDiscussion($priceQuery);
-        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(),$response);
+        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(), $response);
     }
 
     function sendOrderConfirmation()
     {
         $message = $this->rcService->createOrderConfirmation();
-        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(),$message);
+        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(), $message);
     }
 
     function sendDiscountedPriceMessage()
     {
-        if(PriceLog::where(['to' => $this->rcService->getFrom()])->exists())return;
-        PriceLog::create(['to' => $this->rcService->getFrom()]);
+        if (PriceLog::where(['to' => $this->rcService->getFrom(), 'price' => 1])->exists()) return;
+        PriceLog::updateOrCreate(
+            ['to' => $this->rcService->getFrom()],
+            [
+                'price' => 1
+            ]
+        );
         $message = $this->rcService->getDiscountedPriceMessage();
-        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(),$message);
+        return $this->waService->sendWhatsAppMessage($this->rcService->getFrom(), $message);
     }
 
     function sendTestMessage($message)
---
+++ database/migrations/2024_01_24_160541_update_price_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('price_logs', function (Blueprint $table) {
+            $table->integer('price')->default(0);
+            $table->integer('address')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};

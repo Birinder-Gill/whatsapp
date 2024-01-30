440f87d15c3041bdde7687d8cc7617903664c624
Birinder <birindergill53@gmail.com>
2024-01-31 01:58:45 +0530

    production shit

--- app/Http/Controllers/WhatsAppMessageController.php
+++ app/Http/Controllers/WhatsAppMessageController.php
@@ -53,7 +53,7 @@ class WhatsAppMessageController extends Controller
 
     function messageReceived(Request $request)
     {
-        return (request()->json()->all());
+        return json_encode(request()->json()->all());
         try {
 
             $data = request()->json()->all()['data']['message']['_data'];

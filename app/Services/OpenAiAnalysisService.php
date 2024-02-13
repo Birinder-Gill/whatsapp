cf296c54ef946e0253a12507c668b5e0e71e5688
Birinder <birindergill53@gmail.com>
2024-02-13 12:25:31 +0530

    production shit

--- app/Services/OpenAiAnalysisService.php
+++ app/Services/OpenAiAnalysisService.php
@@ -38,6 +38,7 @@ class OpenAiAnalysisService
 
     function queryDetection($message) : GeneralQuery {
         $toSend= $this->createAndRun($message);
+        $toSend = explode("-",$toSend)[0];
         return match ($toSend) {
             'PRICE' => GeneralQuery::PRICE,
             'ADDRESS' => GeneralQuery::ADDRESS,

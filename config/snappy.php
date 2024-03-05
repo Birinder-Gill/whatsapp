00c0546bff582d9d81a022cb69d0bb7cc02f4f51
Birinder <birindergill53@gmail.com>
2024-03-05 12:48:28 +0530

    production shit

--- config/snappy.php
+++ config/snappy.php
@@ -45,7 +45,8 @@ return [
 
     'image' => [
         'enabled' => true,
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
+        'binary' => base_path('vendor\h4cc\wkhtmltoimage-amd64\bin\wkhtmltoimage-amd64'),
+        // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,

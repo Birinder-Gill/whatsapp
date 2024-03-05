bb8bfd5ddb7a1e9888a10f4fcfecee4af60216fe
Birinder <birindergill53@gmail.com>
2024-03-05 20:23:06 +0530

    production shit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\KillSwitchMiddleware::class
-
     ];
 
     /**
@@ -65,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'killSwitch' => \App\Http\Middleware\KillSwitchMiddleware::class
     ];
 }

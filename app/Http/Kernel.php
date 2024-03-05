2a02012da5d1ba633e5ef2aaaba4d32b3428a0ee
Birinder <birindergill53@gmail.com>
2024-03-05 20:15:12 +0530

    production shit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'killSwitch' => \App\Http\Middleware\KillSwitchMiddleware::class
         ],
     ];
 
@@ -63,6 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'killSwitch' => \App\Http\Middleware\KillSwitchMiddleware::class
     ];
 }

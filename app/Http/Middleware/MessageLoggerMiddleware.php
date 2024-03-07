f49f8a36b88c06ae53f06f5eb457da7a3b878e3b
Birinder <birindergill53@gmail.com>
2024-03-07 10:43:13 +0530

    production shit

--- app/Http/Middleware/MessageLoggerMiddleware.php
+++ app/Http/Middleware/MessageLoggerMiddleware.php
@@ -19,47 +19,6 @@ class MessageLoggerMiddleware
     public function handle(Request $request, Closure $next)
     {
 
-        // try {
-        //     $data = request()->json()->all()['data']['message']['_data'];
-        //     $fromMe = $data['id']['fromMe'];
-        //     $message = $data['body'];
-        //     $to = $data['to'];
-        //     $from = $data['from'];
-
-        //     $messageNumber = detectManualMessage($from, $message, $fromMe);
-        //     // if (KillSwitch::where([
-        //     //     "from" => $fromMe ? $to : $from,
-        //     //     "kill" => true,
-        //     // ])->exists()) {
-        //     //     return response("Access denied", 403); // Block the request
-        //     // }
-
-        //     if ($messageNumber > -1 && (!(request()->json()->all()['data']["media"]))) {
-        //         if ($messageNumber === 0 && $fromMe) {
-        //             $message = "Info message......";
-        //         }
-        //         MessageLog::create(
-        //             [
-        //                 "from" => $fromMe ? $to : $from,
-        //                 "fromMe" => $fromMe,
-        //                 "displayName" => ' $personName',
-        //                 "messageText" => $message,
-        //                 "counter" => $messageNumber
-        //             ]
-        //         );
-        //     }
-        // } catch (\Throwable $th) {
-        //     // MessageLog::create(
-        //     //     [
-        //     //         "from" => 'asd',
-        //     //         "fromMe" => true,
-        //     //         "displayName" => ' $personName',
-        //     //         "messageText" => $th->getMessage(),
-        //     //         "counter" => 1
-        //     //     ]
-        //     // );
-        // }
-
 
         return $next($request); // Allow the request to proceed
     }

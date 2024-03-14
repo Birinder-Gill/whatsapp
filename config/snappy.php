a2fdf0fe8cde0bbbbdacfc2690c16f56e333d1b0
Birinder <birindergill53@gmail.com>
2024-03-14 18:56:31 +0530

    production shit

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,8 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
@@ -45,8 +46,8 @@ return [
 
     'image' => [
         'enabled' => true,
-        // 'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
-        'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
+        'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
+        // 'binary' => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,

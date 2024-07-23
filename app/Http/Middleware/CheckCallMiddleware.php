0519859a87746b8db4bf8516fd6d7924108b14a8
Birinder <birindergill53@gmail.com>
2024-07-24 01:58:57 +0530

    production shit

--- app/Http/Middleware/CheckCallMiddleware.php
+++ app/Http/Middleware/CheckCallMiddleware.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\WhatsAppApiService;
 use Closure;
 use Illuminate\Http\Request;
 
 class CheckCallMiddleware
 {
+
+
+
+    protected WhatsAppApiService $waService;
+
+    public function __construct(WhatsAppApiService $waService)
+    {
+        $this->waService = $waService;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -19,10 +30,10 @@ class CheckCallMiddleware
         try {
             $data = request()->json()->all();
             if ($data['event'] === 'call') {
-                // $content =  "*CONFIRM ORDER*\n------------------------\n";
-                // $content = $content.'*Number:* ' . substr(explode("@", $to)[0], -10) . "\n";
-                // $content = $content . '*Address: ' . ($response['address']) . "*\n";
-                //  $this->waService->sendWhatsAppMessage('120363318263831708@g.us', $content);
+                $call = $data['data']['call'];
+                $content =  "*ASK QUERY*\n------------------------\n";
+                $content = $content.'*Number:* ' . substr(explode("@", $call['from'])[0], -10) . "\n";
+                $this->waService->sendWhatsAppMessage('120363318263831708@g.us', $content);
                 logMe("CALL AYA",$data);
                 return response("It is a call", 200);
             }
